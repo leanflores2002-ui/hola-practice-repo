bf83be5e92087d7061bcd9deec52531eae903e6b
leanflores2002-ui <lean.flores2002@gmail.com>
2025-09-09 15:31:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: true,
+    port: 5173,
+    strictPort: true,
+    open: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
